c31421daabdb86578c56d3ee73d3f8599b00d272
John Hendrikx <hjohn@xs4all.nl>
2021-02-12 05:19:37 +0100

    Add javadocs

--- src/main/java/hs/jfx/eventstream/Changes.java
+++ src/main/java/hs/jfx/eventstream/Changes.java
@@ -5,9 +5,20 @@ import hs.jfx.eventstream.impl.RootChangeStream;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 
+/**
+ * Constructs {@link ChangeStream}s.
+ */
 public class Changes {
   private static final ChangeStream<?> EMPTY = new RootChangeStream<>(emitter -> Subscription.EMPTY);
 
+  /**
+   * Constructs a {@link ChangeStream}, with values of type <code>T</code>, from a given {@link ObservableValue}.
+   * The returned stream will emit every change of the observable.
+   *
+   * @param <T> the type of values the stream emits
+   * @param observable an {@link ObservableValue} used as source for the stream, cannot be null
+   * @return a {@link ChangeStream} which uses the given {@link ObservableValue} as source, never null
+   */
   public static <T> ChangeStream<T> of(ObservableValue<T> observable) {
     return new RootChangeStream<>(emitter -> {
       ChangeListener<T> listener = (obs, old, current) -> emitter.emit(current);
@@ -18,6 +29,14 @@ public class Changes {
     });
   }
 
+  /**
+   * Constructs a {@link ChangeStream}, with values of type <code>Change&lt;T&gt;</code>, from a given {@link ObservableValue}.
+   * The returned stream will emit every change of the observable.
+   *
+   * @param <T> the type of {@link Change} the stream emits
+   * @param observable an {@link ObservableValue} used as source for the stream, cannot be null
+   * @return a {@link ChangeStream} which uses the given {@link ObservableValue} as source, never null
+   */
   public static <T> ChangeStream<Change<T>> diff(ObservableValue<T> observable) {
     return new RootChangeStream<>(emitter -> {
       ChangeListener<T> listener = (obs, old, current) -> emitter.emit(new Change<>(old, current));
@@ -28,6 +47,12 @@ public class Changes {
     });
   }
 
+  /**
+   * Constructs an empty {@link ChangeStream} which never emits anything.
+   *
+   * @param <T> the type of values the stream emits
+   * @return a {@link ChangeStream} which never emits anything, never null
+   */
   @SuppressWarnings("unchecked")
   public static <T> ChangeStream<T> empty() {
     return (ChangeStream<T>)EMPTY;
--- src/main/java/hs/jfx/eventstream/Values.java
+++ src/main/java/hs/jfx/eventstream/Values.java
@@ -6,23 +6,33 @@ import hs.jfx.eventstream.impl.Emitter;
 
 import javafx.beans.value.ObservableValue;
 
+/**
+ * Constructs {@link ValueStream}s.
+ */
 public class Values {
 
   /**
-   * Creates an event stream that emits the current value of the given
-   * {@code ObservableValue} immediately for every subscriber and then on
-   * every change.
+   * Constructs a {@link ValueStream}, with values of type <code>T</code>,
+   * from a given {@link ObservableValue}.  The returned stream will
+   * emit the current value of the observable immediately for every subscriber
+   * and then on every change.
    *
-   * @param <T> type of values emitted by the new stream
-   * @param observable an observable which serves as a value source for the new event stream
-   * @return an event stream that emits the value of the given
-   * {@code ObservableValue} immediately for every subscriber and then on
-   * every change
+   * @param <T> the type of values the stream emits
+   * @param observable an {@link ObservableValue} used as source for the stream, cannot be null
+   * @return a {@link ValueStream} which uses the given {@link ObservableValue} as source, never null
    */
   public static <T> ValueStream<T> of(ObservableValue<T> observable) {
     return Changes.of(observable).withDefaultGet(observable::getValue);
   }
 
+  /**
+   * Constructs a {@link ValueStream}, with values of type <code>T</code>,
+   * which emits the given value exactly once upon each subscription.
+   *
+   * @param <T> the type of values the stream emits
+   * @param value a value to emit upon each subscription
+   * @return a {@link ValueStream} which emits the given value exactly once upon each subscription, never null
+   */
   public static <T> ValueStream<T> constant(T value) {
     return Changes.<T>empty().withDefault(value);
   }
