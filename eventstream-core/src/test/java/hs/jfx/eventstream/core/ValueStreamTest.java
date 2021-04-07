8722b70149c9959c3256c322c1bfec881e09c32d
John Hendrikx <hjohn@xs4all.nl>
2021-04-07 02:00:49 +0200

    Extend test

--- eventstream-core/src/test/java/hs/jfx/eventstream/core/ValueStreamTest.java
+++ eventstream-core/src/test/java/hs/jfx/eventstream/core/ValueStreamTest.java
@@ -124,6 +124,8 @@ public class ValueStreamTest {
 
     @Nested
     class ConditionOn {
+      private final Sink<String> strings2 = new Sink<>();
+      private final Sink<String> strings3 = new Sink<>();
 
       @Test
       public void shouldEmitValuesConditionally() {
@@ -138,28 +140,39 @@ public class ValueStreamTest {
         assertEquals(List.of("Bye"), strings.drain());  // expect current value for new subscriber
 
         visible.set(false);
-        property.set("Hello");
 
         assertEquals(List.of(), strings.drain());  // current subscriber gets nothing as condition is false
 
-        stream.subscribe(strings::add);
+        property.set("Hello");
 
-        assertEquals(List.of(), strings.drain());  // new subscriber gets nothing as condition is false
+        assertEquals(List.of(), strings.drain());
+
+        stream.subscribe(strings2::add);
+
+        assertEquals(List.of(), strings.drain());
+        assertEquals(List.of(), strings2.drain());
 
         visible.set(true);
-        assertEquals(List.of("Hello", "Hello"), strings.drain());  // when condition becomes true, both subcribers immediately get notified
+
+        assertEquals(List.of("Hello"), strings.drain());  // when condition becomes true, both subcribers immediately get notified
+        assertEquals(List.of("Hello"), strings2.drain());
 
         property.set("Hi");
 
-        assertEquals(List.of("Hi", "Hi"), strings.drain());  // both subscribers receive new changes while condition is true
+        assertEquals(List.of("Hi"), strings.drain());  // both subscribers receive new changes while condition is true
+        assertEquals(List.of("Hi"), strings2.drain());
 
-        stream.subscribe(strings::add);
+        stream.subscribe(strings3::add);
 
-        assertEquals(List.of("Hi"), strings.drain());  // expect current value for new subscriber
+        assertEquals(List.of(), strings.drain());
+        assertEquals(List.of(), strings2.drain());
+        assertEquals(List.of("Hi"), strings3.drain());  // expect current value for new subscriber
 
         property.set("World");
 
-        assertEquals(List.of("World", "World", "World"), strings.drain());  // all subscribers get current value
+        assertEquals(List.of("World"), strings.drain());  // all subscribers get current value
+        assertEquals(List.of("World"), strings2.drain());
+        assertEquals(List.of("World"), strings3.drain());
       }
 
       @Test
